9fe7edc9e1fc52d6bcbcec9f5518bf80d558804e
Ross Voerman <voermanr@oregonstate.edu>
2023-05-09 10:56:31 -0700

    Added macros for inode record

--- inode.c
+++ inode.c
@@ -1,6 +1,15 @@
 #include "free.h"
 #include "block.h"
 
+#define INODE_OFFSET_SIZE 0
+#define INODE_OFFSET_OWNER_ID 4
+#define INODE_OFFSET_PERMISSIONS 6
+#define INODE_OFFSET_FLAGS 7
+#define INODE_OFFSET_LINK_COUNT
+#define INODE_OFFSET_BLOCK_PTR_START 9
+#define INODE_OFFSET_BLOCK_PTR_SPACING 2
+#define INODE_NUM_BLOCK_PTR_BLOCKS 16
+
 int ialloc(void) {
     unsigned char inode_map[BLOCK_SIZE] = {0};
     bread(BLOCK_INODE_MAP, inode_map);
